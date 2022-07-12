fceb96a797c4c90a9a754b10c007602f36134ceb
Fangyi Zhou <me@fangyi.io>
2022-07-12 22:44:51 +0100

    Add formatting

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,17 @@
-import { Context, APIGatewayProxyResult, APIGatewayEvent } from 'aws-lambda';
-import {APIInteraction, InteractionType} from 'discord-api-types/v10';
+import { Context, APIGatewayProxyResult, APIGatewayEvent } from "aws-lambda";
+import { APIInteraction, InteractionType } from "discord-api-types/v10";
 
-export const lambdaHandler = async (event: APIGatewayEvent, context: Context): Promise<APIGatewayProxyResult> => {
+export const lambdaHandler = async (
+    event: APIGatewayEvent,
+    context: Context
+): Promise<APIGatewayProxyResult> => {
     if (!event.body) {
         return {
             statusCode: 400,
             body: JSON.stringify({
-                message: "Invalid parameters: Request does not have a body."
-            })
-        }
+                message: "Invalid parameters: Request does not have a body.",
+            }),
+        };
     }
     const body = JSON.parse(event.body) as APIInteraction;
     if (body.type === InteractionType.Ping) {
@@ -16,14 +19,14 @@ export const lambdaHandler = async (event: APIGatewayEvent, context: Context): P
         return {
             statusCode: 200,
             body: JSON.stringify({
-                "type": InteractionType.Ping
-            })
-        }
+                type: InteractionType.Ping,
+            }),
+        };
     }
     return {
         statusCode: 200,
         body: JSON.stringify({
-            message: 'hello world',
+            message: "hello world",
         }),
     };
 };
