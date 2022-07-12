e6297b4655294712388b797c31eaa51a5e2f0880
Fangyi Zhou <me@fangyi.io>
2022-07-12 22:39:10 +0100

    Implement ping

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,25 @@
 import { Context, APIGatewayProxyResult, APIGatewayEvent } from 'aws-lambda';
+import {APIInteraction, InteractionType} from 'discord-api-types/v10';
 
 export const lambdaHandler = async (event: APIGatewayEvent, context: Context): Promise<APIGatewayProxyResult> => {
-    console.log(`Event: ${JSON.stringify(event, null, 2)}`);
-    console.log(`Context: ${JSON.stringify(context, null, 2)}`);
+    if (!event.body) {
+        return {
+            statusCode: 400,
+            body: JSON.stringify({
+                message: "Invalid parameters: Request does not have a body."
+            })
+        }
+    }
+    const body = JSON.parse(event.body) as APIInteraction;
+    if (body.type === InteractionType.Ping) {
+        console.log("Responding to a Ping message");
+        return {
+            statusCode: 200,
+            body: JSON.stringify({
+                "type": InteractionType.Ping
+            })
+        }
+    }
     return {
         statusCode: 200,
         body: JSON.stringify({
