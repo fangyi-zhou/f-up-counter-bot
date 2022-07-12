8bd6274418721bc58f7541865ba61c1f725cb0d8
Fangyi Zhou <me@fangyi.io>
2022-07-12 23:07:20 +0100

    Discord verification and ping/pong

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,9 @@
 import { Context, APIGatewayProxyResult, APIGatewayEvent } from "aws-lambda";
 import { APIInteraction, InteractionType } from "discord-api-types/v10";
+import nacl from "tweetnacl";
+import "process";
+
+const PUBLIC_KEY = process.env.PUBLIC_KEY!;
 
 export const lambdaHandler = async (
     event: APIGatewayEvent,
@@ -13,6 +17,25 @@ export const lambdaHandler = async (
             }),
         };
     }
+    console.log("Event %o", event);
+    const signature = event.headers["x-signature-ed25519"]!;
+    const timestamp = event.headers["x-signature-timestamp"]!;
+
+    const isVerified = nacl.sign.detached.verify(
+        Buffer.from(timestamp + event.body),
+        Buffer.from(signature, "hex"),
+        Buffer.from(PUBLIC_KEY, "hex")
+    );
+
+    if (!isVerified) {
+        return {
+            statusCode: 401,
+            body: JSON.stringify({
+                message: "Invalid request signature.",
+            }),
+        };
+    }
+
     const body = JSON.parse(event.body) as APIInteraction;
     if (body.type === InteractionType.Ping) {
         console.log("Responding to a Ping message");
